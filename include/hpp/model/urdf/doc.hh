c1474414dadb358b7a5a6fa5bade9de0616cda61
Florent Lamiraux <florent@laas.fr>
2014-03-29 10:24:19 +0100

    Add some documentation.

---
+++ include/hpp/model/urdf/doc.hh
@@ -0,0 +1,25 @@
+// Copyright (C) 2014 CNRS-LAAS
+// Authors: Florent Lamiraux
+//
+// This file is part of the hpp-model-urdf.
+//
+// hpp-model-urdf is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// hpp-model-urdf is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with hpp-model-urdf.  If not, see <http://www.gnu.org/licenses/>.
+
+/// \mainpage
+///
+/// This package implements an urdf parser that build an instance of
+/// hpp::model::HumanoidRobot.
+///
+/// See function hpp::model::urdf::loadRobotModel for details about how to
+/// load a robot model.
